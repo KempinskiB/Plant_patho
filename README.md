7085b4a0318026ba0335e1c48e3c1c06f8ce5809
roni-kemp <roni.kemp@gmail.com>
2020-09-26 15:56:25 +0300

    rename and init results

--- README.md
+++ README.md
@@ -8,4 +8,9 @@ https://www.kaggle.com/c/plant-pathology-2020-fgvc7
 
 # some thoughts:
 * need to isolate leaves from background - in order to be able to detect their shape
-* edge detection might be a way to do thath but not sure how 
+* edge detection might be a way to do thath but not sure how
+
+
+
+* first attempt doesn't look very good... also my pc is too week for this shit
+33/71 [============>.................] - ETA: 11:07 - loss: 2.0624e-07 - accuracy: 0.3072
--- tens_attempt.py
+++ tens_attempt.py
@@ -10,8 +10,6 @@ from tqdm import tqdm
 from pathlib import Path
 import pandas as pd
 
-DATADIR = "X:/Datasets/PetImages"
-
 repo_dir = Path(".").absolute()
 sample_imgs_dir = repo_dir/"sample_imgs"
 imgs_dir = repo_dir/"all_data"/"images"
@@ -21,7 +19,7 @@ map_data = pd.read_csv(map_file)
 map_data.set_index('image_id',inplace=True)
 map_data = map_data.transpose()
 
-IMG_SIZE = 500
+IMG_SIZE = 300
 
 CATEGORIES = ["healthy","multiple_diseases","rust","scab"]
 
@@ -39,18 +37,22 @@ for img in tqdm(os.listdir(imgs_dir)):  # iterate over each image
     img_array = cv2.imread(os.path.join(imgs_dir,img))  # convert to array
     new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
     training_data.append([new_array, cat_indx])
+
+    #%%
+    cv2.namedWindow("original img", cv2.WINDOW_NORMAL)
+    cv2.imshow("original img", new_array)
 #%%
 ## this part can be done pretier
 X = []
 y = []
 
-for features,label in training_data:
+for features,label in tqdm(training_data):
     if not label ==1:
         X.append(features)
         y.append(label)
 
 X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 3)
-
+y = np.array(y)
 #%%
 
 import pickle
@@ -63,9 +65,45 @@ pickle_out = open("y.pickle","wb")
 pickle.dump(y, pickle_out)
 pickle_out.close()
 
-#%% for loading
+#%%
+
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+
+import pickle
+#%%
 pickle_in = open("X.pickle","rb")
 X = pickle.load(pickle_in)
 
 pickle_in = open("y.pickle","rb")
 y = pickle.load(pickle_in)
+#%%
+X = X/255.0
+#y = y1
+#%%
+model = Sequential()
+
+model.add(Conv2D(256, (3, 3), input_shape=X.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(256, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+
+model.add(Dense(64))
+
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+#%
+model.fit(X, y, batch_size=22, epochs=3, validation_split=0.1)
