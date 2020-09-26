7c70fddd41f088d7ccfddd99eba8f495af1af027
roni-kemp <roni.kemp@gmail.com>
2020-09-26 13:12:21 +0300

    Update tens_hello_world.py

--- tens_hello_world.py
+++ tens_hello_world.py
@@ -13,7 +13,8 @@ import pandas as pd
 DATADIR = "X:/Datasets/PetImages"
 
 repo_dir = Path(".").absolute()
-imgs_dir = repo_dir/"sample_imgs"
+sample_imgs_dir = repo_dir/"sample_imgs"
+imgs_dir = repo_dir/"all_data"/"images"
 map_file = repo_dir/"raw_data"/"train.csv"
 
 map_data = pd.read_csv(map_file)
@@ -29,6 +30,8 @@ training_data = []
 for img in tqdm(os.listdir(imgs_dir)):  # iterate over each image
 
     # get the img category, and its index
+    if "test" in img.lower():
+        continue
     img_id = img.strip(".jpg")
     category = map_data[img_id][map_data[img_id]!=0].index[0]
     cat_indx = CATEGORIES.index(category)
@@ -36,16 +39,33 @@ for img in tqdm(os.listdir(imgs_dir)):  # iterate over each image
     img_array = cv2.imread(os.path.join(imgs_dir,img))  # convert to array
     new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
     training_data.append([new_array, cat_indx])
-
+#%%
 ## this part can be done pretier
 X = []
 y = []
 
 for features,label in training_data:
-    X.append(features)
-    y.append(label)
+    if not label ==1:
+        X.append(features)
+        y.append(label)
 
 X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 3)
 
+#%%
 
+import pickle
 
+pickle_out = open("X.pickle","wb")
+pickle.dump(X, pickle_out)
+pickle_out.close()
+
+pickle_out = open("y.pickle","wb")
+pickle.dump(y, pickle_out)
+pickle_out.close()
+
+#%% for loading
+pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
+
+pickle_in = open("y.pickle","rb")
+y = pickle.load(pickle_in)
