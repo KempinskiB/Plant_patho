eb577b6a4b247b58d42dd2254df08223d4bdb90b
roni-kemp <roni.kemp@gmail.com>
2020-09-27 13:21:19 +0300

    adding multiply func
    to make data set biger we can rotate and flip imgs (right?)

--- img_prosessing.py
+++ img_prosessing.py
@@ -35,6 +35,9 @@ cv2.imshow("original img", img1)
 x = largest.max(axis = 0)[0][0]
 y = largest.max(axis = 0)[0][1]
 
+x = largest.min(axis = 0)[0][0]
+y = largest.min(axis = 0)[0][1]
+
 cv2.circle(color,(x,y),10,(0,0,255),-1)
 cv2.imshow("dilation", color)
 
@@ -42,15 +45,31 @@ cv2.imshow("dilation", color)
 ret,thresh = cv2.threshold(blur,110,255,0)
 
 #%% "main" sort of
+import imutils
+def resize_and_multiply_by_rotate(img, ):
+    out_folder = repo_dir/"rotated_and_resized"
+    IMG_SIZE = 300
+    img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+    cv2.imshow('Original image', img)
+    for theta in [90,180,270]:
+#        out_path=out_folder/str(theta)
+        rotated = imutils.rotate(img, 45)
 
-def multiply_by_rotate():
-    pass
+        cv2.imshow(str(theta), rotated)
+        if theta == 90:
+            flipVertical = cv2.flip(rotated, 0)
+            flipHorizontal = cv2.flip(rotated, 1)
+
+    flipVertical = cv2.flip(img, 0)
+    flipHorizontal = cv2.flip(img, 1)
+    cv2.imshow('vertical image', flipVertical)
+    cv2.imshow('horizontal image', flipHorizontal)
+
+#        cv2.imwrite(out_path, rotated)
 
 
 
 def find_leaf_contour(img):
-    IMG_SIZE = 400
-    img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
     edges = cv2.Canny(img,50,100)
     kernel = np.ones((9,9),np.uint8)
     dilation = cv2.dilate(edges,kernel,iterations = 1)
@@ -71,10 +90,13 @@ for i in range(1, len(img_lst)):
 
     img1_path = os.path.join(imgs_dir, img_lst[i])
     img = cv2.imread(img1_path,1)
+#    IMG_SIZE = 300
+#    img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
 
     find_leaf_contour(img)
     if cv2.waitKey(-1) == ord('a'):
         print("a")
+        break
 
 
 
